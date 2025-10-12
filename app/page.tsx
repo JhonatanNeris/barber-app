31a65138db052313f32b3f9aa63061a178a3695d
Jhonatan Neris <jhonatansnx@gmail.com>
2025-10-12 12:55:21 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "./components/ui/button";
+import { Button } from "./components/ui/button"
 
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-blue-500 text-6xl hover:scale-125">Oi</h1>
       <Button>Teste Shacn button</Button>
     </div>
-  );
+  )
 }
