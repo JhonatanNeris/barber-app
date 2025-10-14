d44c656dbb3a863b0325f4cbf62eeb1293bd0564
Jhonatan <jhonatansnx@gmail.com>
2025-10-14 14:06:11 -0300

    feat: implement quick search options and booking item component

---
+++ app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  iconUrl: string
+  label: string
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    label: "Cabelo",
+    iconUrl: "/cabelo-icon.svg",
+  },
+  {
+    label: "Barba",
+    iconUrl: "/barba-icon.svg",
+  },
+  {
+    label: "Acabamento",
+    iconUrl: "/acabamento-icon.svg",
+  },
+  {
+    label: "Sobrancelha",
+    iconUrl: "/sobrancelha.svg",
+  },
+  {
+    label: "Massagem",
+    iconUrl: "/massagem.svg",
+  },
+  {
+    label: "Hidratação",
+    iconUrl: "/hidratacao.svg",
+  },
+]
